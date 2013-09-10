63ad4137da937625331930f3a0b320f6e3dc90e6
Pauli Perälä <perala.pauli@gmail.com>
2013-09-10 22:02:30 +0300

    added average ratings
    added new method to Beer class and added view to show it

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,7 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+	def average_rating
+		self.ratings.map{|rating| rating.score}.reduce(:+).to_f / self.ratings.count
+	end
 end
