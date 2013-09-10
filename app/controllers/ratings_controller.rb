2e077390e415179a1373d2486db89496dbb0ccff
Pauli Perälä <perala.pauli@gmail.com>
2013-09-11 00:05:34 +0300

    extended ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,13 +1,32 @@
 class RatingsController < ApplicationController
-	before_action :set_rating, only: []
+	before_action :set_rating, only: [:destroy]
+
 	def index
 		@ratings = Rating.all
 	end
+	def new 
+		@beers = Beer.all
+		@rating = Rating.new
+	end
+
+	def create 
+		@rating = Rating.create(rating_params)
+		if @rating.save
+			redirect_to ratings_path
+		end
+	end
+
+	def destroy
+		@rating.destroy
+		redirect_to ratings_path
+	end
+	
 	private
 	def set_rating
 		@rating = Rating.find(params[:id])
 	end
-	def beer_params
-		params.require(:beer).permit(:score,:beer_id)
+
+	def rating_params
+		params.require(:rating).permit(:score,:beer_id)
 	end
 end
