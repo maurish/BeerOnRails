edf3c98c918e38f6053b4e1dc4d7a463401bb836
Pauli Perälä <perala.pauli@gmail.com>
2013-09-11 00:01:52 +0300

    Updated Beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,11 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
-	has_many :ratings
+	has_many :ratings, dependent: :destroy
 	def average_rating
 		self.ratings.map{|rating| rating.score}.reduce(:+).to_f / self.ratings.count
+	end
+
+	def to_s
+		"#{name} #{brewery.name}"
 	end
 end
