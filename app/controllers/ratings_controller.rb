9acb2cd8de839ef0133707513ad54d4ce2a2503b
Pauli Perälä <perala.pauli@gmail.com>
2013-09-10 18:57:43 +0300

    Model Rating to db
    added model ratign and created oneToMany between Beer

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,10 @@
 class RatingsController < ApplicationController
+	before_action :set_rating, only: []
+	private
+	def set_rating
+		@rating = Rating.find(params[:id])
+	end
+	def beer_params
+		params.require(:beer).permit(:score,:beer_id)
+	end
 end
