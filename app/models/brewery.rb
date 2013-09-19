1175459b089b3a1abfd72b7877d302136e35339e
Pauli Perälä <perala.pauli@gmail.com>
2013-09-20 00:23:54 +0300

    brewery validation

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,8 +2,17 @@ class Brewery < ActiveRecord::Base
 	has_many :beers, dependent: :destroy
 	has_many :ratings, through: :beers
 
+	validates :name,:year,  presence: true
+	validates :year, numericality: true
+	validate :prevent_future
+
 	include Average
 	
+	def prevent_future
+		errors.add :year,  "Founding year can't be in the future!" if Time.now.year < year	or year < 1042
+	end
+
+
 	def to_s
 		"#{name} :#{year}"
 	end
