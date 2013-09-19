079d4fd4df450fd8d5d3548e1d9835a778101036
Pauli Perälä <perala.pauli@gmail.com>
2013-09-19 15:33:05 +0300

    changed navigation link helper
    changed method signature to accept htmlHash, to support more link_to
    methods

--- app/helpers/nav_link_helper.rb
+++ app/helpers/nav_link_helper.rb
@@ -1,10 +1,10 @@
 module NavLinkHelper
 	# @TODO change method signature to accept hash of options to support class etc easier
-	def nav_link(link_text, link_path, method = :get)
+	def nav_link(link_text, link_path,html_options = {})
 	  class_name = current_page?(link_path) ? 'current' : ''
 
 	  content_tag(:li, :class => class_name) do
-	    link_to link_text, link_path, method: method
+	    link_to link_text, link_path, html_options
 	  end
 	end
 end
