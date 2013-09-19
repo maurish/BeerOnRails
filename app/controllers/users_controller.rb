199e20c5385d368475166b5ee53479b67c0f9fcf
Pauli Perälä <perala.pauli@gmail.com>
2013-09-19 15:41:53 +0300

    added user to rating relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :check_priviliges, only: [:update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -25,7 +26,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -40,6 +40,8 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+
+
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -54,6 +56,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    reset_session
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url }
@@ -66,6 +69,10 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
+  def check_priviliges
+    redirect_to :back, alert: "You don't have enough priviliges to do that" unless @user==current_user
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
