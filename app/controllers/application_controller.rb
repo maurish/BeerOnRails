107e89c618d0c2a1d21297a301582771d1983b0a
Pauli Perälä <perala.pauli@gmail.com>
2013-09-19 16:56:33 +0300

    added method currently_signed_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	helper_method :current_user, :currently_signed_in?
 
-  helper_method :current_user
-  def current_user
-  	return nil if session[:user_id].nil?
-  	User.find(session[:user_id])
-  end
+  	def current_user
+  		return nil if session[:user_id].nil?
+  		User.find(session[:user_id])
+  	end
 
+	def currently_signed_in?(user)
+		user == current_user
+	end
   
 end
