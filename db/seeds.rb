ba52fbcb93edbb73172af13ba5b625b7b8eaab58
Pauli Perälä <perala.pauli@gmail.com>
2014-02-16 23:07:23 +0200

    remade seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,25 @@ c3 = BeerClub.create name: "Suuri Suomalainen Kaljakerho", city: "Turku ", found
 u1 = User.create username: "mauri", password: "pauli1", password_confirmation: "pauli1"
 u2 = User.create username: "mluukkai", password: "matti1", password_confirmation: "matti1"
 u3 = User.create username: "avihavai", password: "arto1", password_confirmation: "arto1"
-u4 = User.create username: "Paula", password: "paula1", password_confirmation: "paula1 "
+u4 = User.create username: "Paula", password: "paula1", password_confirmation: "paula1"
 
-beer1_1 = b1.beers.create :name => "Iso 3", :style => "Lager"
-beer1_2 = b1.beers.create :name => "Karhu", :style => "Lager"
-beer1_3 = b1.beers.create :name => "Tuplahumala", :style => "Lager"
-beer2_1 = b2.beers.create :name => "Huvila Pale Ale", :style => "Pale Ale"
-beer2_2 = b2.beers.create :name => "X Porter", :style => "Porter"
-beer3_1 = b3.beers.create :name => "Hefezeizen", :style => "Weizen"
-beer3_2 = b3.beers.create :name => "Helles", :style => "Lager"
+lager = Style.create title:'Lager', description: 'This is a description for Lager beer'
+pale_ale = Style.create title:'Pale Ale', description: 'This description is for Pale Ale'
+porter = Style.create title:'porter', description: 'This description belongs to porter'
+weizen =  Style.create title:'Weizen', description: 'Weizen description over here, yo!'
 
-s1 = beer1_1.ratings.create :score => 100, user_id: u1.id
-s2 = beer1_2.ratings.create :score => 75 , user_id: u1.id
-s3 = beer2_1.ratings.create :score => 50 , user_id: u2.id
-s4 = beer2_1.ratings.create :score => 61 , user_id: u2.id
+beer1_1 = b1.beers.create :name => "Iso 3", :style => lager
+beer1_2 = b1.beers.create :name => "Karhu", :style => lager
+beer1_3 = b1.beers.create :name => "Tuplahumala", :style => lager
+beer2_1 = b2.beers.create :name => "Huvila Pale Ale", :style => pale_ale
+beer2_2 = b2.beers.create :name => "X Porter", :style => porter
+beer3_1 = b3.beers.create :name => "Hefezeizen", :style => weizen
+beer3_2 = b3.beers.create :name => "Helles", :style => lager
+
+s1 = beer1_1.ratings.create :score => 50, user_id: u1.id
+s2 = beer1_2.ratings.create :score => 45 , user_id: u1.id
+s3 = beer2_1.ratings.create :score => 30 , user_id: u2.id
+s4 = beer2_1.ratings.create :score => 25 , user_id: u2.id
 
 m1 = c2.memberships.create user_id: u1.id
 m2 = c2.memberships.create user_id: u2.id
